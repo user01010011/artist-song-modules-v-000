6a2dd7f8717ae5f2335f9c87905d6d60cd8fa45e
Shasha Yan <shashayan@gmail.com>
2021-05-18 21:31:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,4 @@
 require 'pry'
-require_relative '../lib/concerns/findable'
-require_relative '../lib/concerns/memorable'
-require_relative '../lib/concerns/paramable'
 
 class Song
   attr_accessor :name
